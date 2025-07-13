ead63302efe39ffd0084b96531cccf6e7cebe540
rhoeunbin <nhodq219@gmail.com>
2025-07-13 17:50:54 +0900

    ✨add jwt security

---
+++ src/main/java/com/rhonn/board_back/dto/response/ResponseDto.java
@@ -0,0 +1,23 @@
+package com.rhonn.board_back.dto.response;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.rhonn.board_back.common.ResponseCode;
+import com.rhonn.board_back.common.ResponseMessage;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ResponseDto {
+
+    private String code;
+    private String message;
+
+    public static ResponseEntity<ResponseDto> databaseError() {
+        ResponseDto responseBody = new ResponseDto(ResponseCode.DATABASE_ERROR, ResponseMessage.DATABASE_ERROR);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseBody);
+    }
+}
---
+++ src/main/java/com/rhonn/board_back/provider/JwtProvider.java
@@ -0,0 +1,47 @@
+package com.rhonn.board_back.provider;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
+public class JwtProvider {
+
+    @Value("${secret-key}")
+    private String secretKey;
+
+    public String create(String email) {
+
+        Date expireDate = Date.from(Instant.now().plus(1, ChronoUnit.HOURS));
+
+        String jwt = Jwts.builder()
+                .signWith(SignatureAlgorithm.ES256, secretKey)
+                .setSubject(email).setIssuedAt(new Date()).setExpiration(expireDate)
+                .compact();
+
+        return jwt;
+
+    }
+
+    public String validate(String jwt) {
+
+        Claims claims = null;
+
+        try {
+            claims = Jwts.parser().setSigningKey(secretKey)
+                    .parseClaimsJws(jwt).getBody();
+        } catch (Exception e) {
+            return null;
+        }
+        return claims.getSubject();
+    }
+}
